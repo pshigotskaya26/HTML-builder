78d31c154e9e5bc283ff4601e41a1f717aba5a67
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-11-02 05:52:23 +0300

    feat: add solution of task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = process;
+
+const pathDirname = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(`${pathDirname}`, 'utf-8');
+
+let data = '';
+
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => stdout.write(data));
