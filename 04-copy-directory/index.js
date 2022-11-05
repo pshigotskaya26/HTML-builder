a8dd36f0a90dc067c1d8cb3720af1991bf286a61
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2022-11-05 08:10:15 +0300

    feat: add solution of the task named 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathDirName = path.dirname(__filename);
+const pathCurrentFolder = path.join(__dirname, './files');
+const pathCreatedFolder = path.join(pathDirName, './files-copy');
+
+fs.readdir(pathCreatedFolder, {withFileTypes: true}, (err, files) => {
+	if (typeof(files) !== 'undefined') {
+		files.forEach(file => {
+			fs.unlink(path.join(pathCreatedFolder, file.name), (err) => {
+				if (err) throw err;
+			});
+		});
+	}
+});
+
+//create copy of folder named 'files-copy'
+fs.mkdir(pathCreatedFolder, {recursive: true}, () => {
+});
+
+fs.readdir(pathCurrentFolder, {withFileTypes: true}, (err, files) => {
+	files.forEach(file => {
+		fs.copyFile(path.join(pathCurrentFolder, file.name), path.join(pathCreatedFolder, file.name), () => {
+		});
+	});
+});
+
+
 
